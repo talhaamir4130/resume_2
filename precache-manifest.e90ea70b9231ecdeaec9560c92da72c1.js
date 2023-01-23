ebaba6ff18deec2f12c30e360035a697610d5e42
Talha Amir <talhaamir2014@gmail.com>
2023-01-23 08:02:24 +0000

    Updates

---
+++ precache-manifest.e90ea70b9231ecdeaec9560c92da72c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "60583aaadbb0491b3602",
+    "url": "/resume_2/static/css/main.0d82af0e.chunk.css"
+  },
+  {
+    "revision": "60583aaadbb0491b3602",
+    "url": "/resume_2/static/js/main.60583aaa.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/resume_2/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/resume_2/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "dedaef3a7661bd0890bd",
+    "url": "/resume_2/static/js/runtime~main.dedaef3a.js"
+  },
+  {
+    "revision": "a9de102f9d822ed946626657f99b0542",
+    "url": "/resume_2/index.html"
+  }
+];
